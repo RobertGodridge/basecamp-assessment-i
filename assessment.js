87f4afdb74f99d3c2df0437282f8ce07e519fc09
Robert Godridge <robertgodridge@Roberts-MacBook-Pro.local>
2018-12-07 16:36:04 -0700

    change 2

--- assessment.js
+++ assessment.js
@@ -1,6 +1,6 @@
 // #1 create a variable called 'myName' and assign it to your name as a string
 
-CODE HERE = this is the change
+CODE HERE = this is the change change
 
 // #2 create a variable called 'faveNum' and assign it to your favorite number
 
